86ef4cdf62b59d84f08047928780f850a6cc0bd0
kotaro_sakae <ksakae1216@gmail.com>
2019-08-10 18:23:59 +0900

    during implemetion

---
+++ src/main/java/jp/org/web/controller/ListController.java
@@ -0,0 +1,44 @@
+package jp.org.web.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.org.web.form.LoginForm;
+import jp.org.web.repository.LoginRepository;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class ListController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ListController.class);
+	
+	@Autowired
+	private LoginRepository loginRepository;
+	
+	@ModelAttribute
+	public LoginForm setLoginForm() {
+		LoginForm loginForm = new LoginForm();
+		return loginForm;
+	}
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/01_list/list", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Login screen display");
+		
+		return "01_list/list";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/01_list/list.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>list</title>
+  
+</head>
+
+<body>
+list画面いいい
+</body>
+
+</html>
