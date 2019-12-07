0b09d34b87023dabadb713cf90327a9c6e25a4ea
kotaro_sakae <ksakae1216@gmail.com>
2019-12-07 15:25:18 +0900

    complete

--- src/main/java/jp/org/web/controller/IndexController.java
+++ src/main/java/jp/org/web/controller/IndexController.java
@@ -4,19 +4,10 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import jp.org.web.form.LoginForm;
-import jp.org.web.message.LoginMessage;
-import jp.org.web.repository.LoginRepository;
-
 
 /**
  * Handles requests for the application home page.
