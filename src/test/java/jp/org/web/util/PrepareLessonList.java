9bafa15f79a542573652ad1ad7206c540aca638a
kotaro_sakae <ksakae1216@gmail.com>
2019-12-08 10:20:40 +0900

    complete UpdateControllerTest

--- src/test/java/jp/org/web/util/PrepareLessonList.java
+++ src/test/java/jp/org/web/util/PrepareLessonList.java
@@ -15,14 +15,18 @@ public class PrepareLessonList {
         listForm1.setUserFirstName("taro");
         listForm1.setUserLastName("Yamada");
         listForm1.setLesson1st("Java");
+        listForm1.setInformation1st("Javaの情報");
         listForm1.setLesson2nd("Go");
+        listForm1.setInformation2nd("Goの情報");
 
         LessonListForm listForm2 = new LessonListForm();
         listForm2.setUserId("002");
         listForm2.setUserFirstName("jiro");
         listForm2.setUserLastName("Yamada");
         listForm2.setLesson1st("PHP");
+        listForm2.setInformation1st("PHPの情報");
         listForm2.setLesson2nd("Angular");
+        listForm2.setInformation2nd("Angularの情報");
 
         List<LessonListForm> lessonListFormList = new ArrayList<>();
         lessonListFormList.add(listForm1);
